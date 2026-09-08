40a337a10678b677690c1102cd075ce0d50e2a02
Nava Hurwitz <navahurwitz@gmail.com>
2026-09-08 10:53:54 -0400

    added insert statements to populate sql table RabbiInfo

---
+++ database/seed_data.sql
@@ -0,0 +1,16 @@
+INSERT INTO RabbiInfo (Rabbi_id, Rabbi_Acronym, Rabbi_FullName, Rabbi_YearBorn, Rabbi_YearDied, Location)
+VALUES(1, 'Rashi', 'Rav Shlomo Yitzchaki', 1040, 1105, 'France'),
+(2, 'Ibn Ezra', 'Rav Avraham ben Meir Ibn Ezra', 1089, 1167, 'Spain'),
+(3, 'Rambam', 'Rav Moshe ben Maimon', 1135, 1204, 'Spain and Egypt'),
+(4, 'Ramban','Rav Moshe ben Nachman', 1194, 1270, 'Spain and Israel'),
+(5, 'Shadal', 'Rav Shmuel Dovid Luzzato', 1800, 1865, 'Italy'),
+(6, 'Radak', 'Rav Dovid Kimchi', 1160, 1235, 'Spain and France'),
+(7, 'Abarbanel', 'Don Yitzchak Abarbanel', 1437, 1508, 'Portugal and Italy'),
+(8, 'Malbim', 'Rav Meir Leibesh ben Yechiel Michel Wisser', 1809, 1879, 'Ukraine, Poland, and Romania'),
+(9, 'Seforno', 'Rav Ovadia ben Yaakov Seforno', 1475, 1550, 'Italy'),
+(10, 'Alshich', 'Rav Moshe Alshich', 1507, 1600, 'Turkey and Tzfat'),
+(11, 'Chizkuni', 'Rav Chizkiya ben Manoach', 1250, 1310, 'France'),
+(12, 'Netziv', 'Rav Naftali Tzvi Yehuda Berlin', 1816, 1893, 'Belarus and Poland'),
+(13, 'Rashbam', 'Rav Shmuel ben Meir', 1085, 1158, 'France'),
+(14, 'Ralbag', 'Rav Levi ben Gershon', 1288, 1344, 'France'), 
+(15, 'Kli Yakar', 'Rav Shlomo Ephraim ben Aaron Luntschitz', 1550, 1619, 'Poland and Czech Republic');
